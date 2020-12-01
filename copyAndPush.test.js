b80f8bb7e033dfd28462d1af2edc6d3fc2532ed9
kevinfiero <kjf46@cornell.edu>
2020-12-01 13:55:08 -0800

    copyAndPush test

---
+++ copyAndPush.test.js
@@ -0,0 +1,14 @@
+const { copyAndPush } = require('./copyAndPush');
+
+describe('copyAndPush function', () => {
+    it('return new array with pushed item', () => {
+        const expected = [1, 2, 3, 4]
+        const numbers = [1, 2, 3]
+        const result = copyAndPush(numbers, 4)
+        expect(result).toEqual(expected)
+    })
+
+
+
+
+});
