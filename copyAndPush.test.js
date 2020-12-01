3a29aa4dd5c9f5d5b7c8654dd56ecaf9082d5bd1
kevinfiero <kjf46@cornell.edu>
2020-12-01 14:10:01 -0800

    add test to check if original array unchanged

--- copyAndPush.test.js
+++ copyAndPush.test.js
@@ -5,8 +5,13 @@ describe('copyAndPush function', () => {
         const expected = [1, 2, 3, 4]
         const numbers = [1, 2, 3]
         const result = copyAndPush(numbers, 4)
-        console.log(numbers);
         expect(result).toEqual(expected)
+    })
+    it('original array is unchanged', () => {
+        const expected = [1, 2, 3]
+        const numbers = [1, 2, 3]
+        copyAndPush(numbers, 4)
+        expect(numbers).toEqual(expected)
     })
     
 });
