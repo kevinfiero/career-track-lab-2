7289cf405629ad82bf5b04488b76938c34619bf0
kevinfiero <kjf46@cornell.edu>
2020-12-01 15:50:19 -0800

    fetchQuotes test

---
+++ fetchQuotes.test.js
@@ -0,0 +1,17 @@
+const { fetchQuotes } = require('/fetchQuotes');
+
+describe('fetchQuotes', () =>{
+
+    it('retrieve single Futurama quote', () =>  {
+        const expected =   {
+            "character": "Bender",
+            "quote": "I'm a fraud. A poor, lazy, sexy fraud.",
+            "image": "https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png"
+          }
+
+        const result = fetchQuotes();
+        expect(result).toEqual(expected);
+    });
+
+
+});
