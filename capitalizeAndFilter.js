d3fd04cc4fc4de83791095c17dfe445cac3d03e8
kevinfiero <kjf46@cornell.edu>
2020-12-01 14:50:59 -0800

    capitalizeAndFilter code using for loop

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,4 +1,15 @@
-
+const capitalizeAndFilter = pokemon => {
+    const length = pokemon.length;
+    let i = length-1;
+    console.log(length);
+    for(i; i>=0; i--){
+        pokemon[i] = pokemon[i].charAt(0).toUpperCase() + pokemon[i].slice(1);
+        if(pokemon[i].charAt(0)==='F'){
+            pokemon.splice(i, 1)
+        }
+    }
+    return pokemon;
+}
 
 module.exports = {
     capitalizeAndFilter
