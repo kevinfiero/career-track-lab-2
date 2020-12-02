bb2c3cc8b64abe2014ce519b54a174c5f9b95d69
kevinfiero <kjf46@cornell.edu>
2020-12-01 16:49:10 -0800

    added mock data for testing Futurama

--- fetchQuotes.test.js
+++ fetchQuotes.test.js
@@ -1,10 +1,13 @@
 const { fetchQuotes } = require('./fetchQuotes');
+const { quoteData } = require('./quoteData.js')
 const request = require('superagent');
+
+jest.mock('superagent');
 
 describe('fetchQuotes', () =>{
 
     it('retrieve single Futurama quote', async() =>  {
-
+        request.get.mockResolvedValue(quoteData)
         const expected =   {
             character: expect.any(String),
             quote: expect.any(String),
